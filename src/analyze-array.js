8ffeed3369834dccce9e114ff40b3a9170571af6
Orelba <orel450@gmail.com>
2023-02-24 06:19:34 +0200

    Add analyze array function, all tests pass

--- src/analyze-array.js
+++ src/analyze-array.js
@@ -1,5 +1,14 @@
 const analyzeArray = arr => {
+  const average = arr.reduce(
+    (prevNum, currentNum) => prevNum + currentNum
+  ) / 4
 
+  return {
+    average: average,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: arr.length
+  }
 }
 
 export default analyzeArray
--- src/analyze-array.test.js
+++ src/analyze-array.test.js
@@ -1,5 +1,21 @@
 import analyzeArray from './analyze-array'
 
-test('', () => {
-  
+test('Average of all numbers in an array is correct', () => {
+  const expected = { average: 4 }
+  expect(analyzeArray([5, 2, 6, 3])).toMatchObject(expected)
+})
+
+test('Min number from array is correct', () => {
+  const expected = { min: 2 }
+  expect(analyzeArray([5, 2, 13])).toMatchObject(expected)
+})
+
+test('Max number from array is correct', () => {
+  const expected = { max: 8 }
+  expect(analyzeArray([5, 2, 8, 1])).toMatchObject(expected)
+})
+
+test('Array length is correct', () => {
+  const expected = { length: 6 }
+  expect(analyzeArray([1, 2, 3, 4, 5, 6])).toMatchObject(expected)
 })
